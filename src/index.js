54ad1fbb661f67187e4e1ab77693a1e3a91a211f
Supamaero <muwede6@gmail.com>
2020-08-09 18:24:47 +0100

    new update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import 'normalize.css';
 import Main from './main';
 
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Project from './project';
-import 'normalize.css';
+
 
 
 
